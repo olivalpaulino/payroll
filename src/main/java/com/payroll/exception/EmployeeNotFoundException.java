8ddf6962be641aa51e8809f3160e35a1a978b7e6
Olival <olivalpaulinocc@gmail.com>
2023-05-07 12:39:38 -0300

    aula 01 do treinamento Spring framework

---
+++ src/main/java/com/payroll/exception/EmployeeNotFoundException.java
@@ -0,0 +1,10 @@
+package com.payroll.exception;
+
+
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+  public EmployeeNotFoundException(Long id) {
+    super("Could not find employee " + id);
+  }
+}
